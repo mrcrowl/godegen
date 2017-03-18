a8fab0c382dd08a36e3a87a8cf9f25d3afb6b4e4
Ben Crowl <ben@educationperfect.com>
2017-03-18 22:40:55 +1300

    Filled out ServiceDescription structs

--- description/ServiceDescription.go
+++ description/ServiceDescription.go
@@ -1,3 +1,57 @@
 package description
 
-type ServiceDescription struct{}
+type ServiceDescription struct {
+	Namespaces []*Namespace
+}
+
+type Namespace struct {
+	Name       string
+	FullName   string
+	Namespaces []*Namespace
+	Services   []*Service
+}
+
+type Service struct {
+	Name      string
+	Namespace string
+	FullName  string
+	Methods   []*Method
+}
+
+type Method struct {
+	Name          string
+	TypeName      string
+	TypeNamespace string
+	Args          []*Arg
+}
+
+type Arg struct {
+	Name                 string
+	TypeName             string
+	TypeNamespace        string
+	ElementTypeName      string
+	ElementTypeNamespace string
+}
+
+type DataType struct {
+	Name                 string
+	Namespace            string
+	FullName             string
+	BaseTypeName         string
+	BaseTypeNamespace    string
+	ElementTypeName      string
+	ElementTypeNamespace string
+}
+
+type Field struct {
+	Name       string
+	Type       string
+	SourceType string
+}
+
+type Const struct {
+	Name       string
+	Type       string
+	SourceType string
+	Value      string
+}
