8f8a8740deb85395640fb91591bc76ab336685dd
Ben Crowl <ben@educationperfect.com>
2017-03-18 22:50:36 +1300

    Refactor before description phase

--- description/ServiceDescriber.go
+++ description/ServiceDescriber.go
@@ -1,19 +1,29 @@
 package description
 
+import (
+	"fmt"
+	"godegen/reflect"
+)
+
 type ServiceDescriber struct {
+	assemblyFile *reflect.Assembly
 }
 
-func NewServiceDescriber() *ServiceDescriber {
-	return &ServiceDescriber{}
+func NewServiceDescriber(assemblyFilepath string, assemblyName string) *ServiceDescriber {
+	loader := reflect.NewAssemblyLoader(assemblyFilepath)
+	assemblyFile, _ := loader.Load(assemblyName)
+	return &ServiceDescriber{assemblyFile}
 }
 
-func (res *ServiceDescriber) Describe(serviceTypeName string, assemblyFilepath string) (*ServiceDescription, error) {
-
-	// serviceType := assembly.GetType(serviceTypeName)
-	// if serviceType == nil {
-	// 	return nil, errors.New("Type '" + serviceTypeName + "' not found in: " + assemblyFilepath)
-	// }
-
-	// //TODO
+func (res *ServiceDescriber) Describe(serviceTypeName string) (*ServiceDescription, error) {
+	serviceType := res.assemblyFile.GetType(serviceTypeName)
+	resolver := NewServiceTypesResolver(res.assemblyFile)
+	resolvedTypes := resolver.Resolve(serviceType)
+	for _, t := range resolvedTypes {
+		fmt.Println(t.FullName() + ":")
+		for _, field := range t.GetFields() {
+			fmt.Println("\t" + field.Name() + ": " + field.Type().FullName())
+		}
+	}
 	return nil, nil
 }
--- main.go
+++ main.go
@@ -1,52 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"godegen/description"
-	"godegen/reflect"
 )
 
 func main() {
-	loader := reflect.NewAssemblyLoader(`C:\WF\LP\server\EBS_Deployment\bin`)
-	assemblyFile, _ := loader.Load("Classes")
-	// assemblyFile.Test()
-	//assemblyFile, _ := loader.Load("LiteCASClient")
-
-	// type1 := assemblyFile.GetType("nz.co.LanguagePerfect.Services.Classes.BusinessObjects.ClassDescription")
-	str := description.NewServiceTypesResolver(assemblyFile)
-	// ssoRego := assemblyFile.GetType("nz.co.LanguagePerfect.Services.LPLogin.Managers.SSORegistrationManager")
-	ssoRego := assemblyFile.GetType("nz.co.LanguagePerfect.Services.Portals.ControlPanel.LanguageDataPortal")
-	resolvedTypes := str.Resolve(ssoRego)
-	for _, t := range resolvedTypes {
-		fmt.Println(t.FullName() + ":")
-		for _, field := range t.GetFields() {
-			fmt.Println("\t" + field.Name() + ": " + field.Type().FullName())
-		}
-	}
-
-	// utp := assemblyFile.GetType("nz.co.LanguagePerfect.Services.Sessions.BusinessObjects.LPSession").(*reflect.TypeDef)
-
-	// methods := utp.GetMethods()
-	// for _, method := range methods {
-	// 	fmt.Println(method)
-	// }
-
-	// fmt.Println("--TYPEDEF--")
-	// for _, row := range metadata.Tables.GetRows(cli.TableIdxTypeDef) {
-	// 	fmt.Println(row)
-	// }
-
-	// fmt.Println()
-	// fmt.Println("--FIELD--")
-
-	// for _, row := range metadata.Tables.GetRows(cli.TableIdxField) {
-	// 	fmt.Println(row)
-	// }
-
-	// fmt.Println()
-	// fmt.Println("--METHODDEF--")
-
-	// for _, row := range assemblyFile.Tables.GetRows(cli.TableIdxMethodDef) {
-	// 	fmt.Println(row)
-	// }
+	describer := description.NewServiceDescriber(`C:\WF\LP\server\EBS_Deployment\bin`, `Classes`)
+	describer.Describe("nz.co.LanguagePerfect.Services.Portals.ControlPanel.LanguageDataPortal")
 }
