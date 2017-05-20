bd6da3969c31489d2e57df1ccaae388ea09fa451
Ben Crowl <ben@educationperfect.com>
2017-05-20 12:00:48 +1200

    Update build process

--- build.bat
+++ build.bat
@@ -1,1 +1,4 @@
+@echo off
+echo Building
+go get
 go build -o codegen.exe -ldflags "-s -w"
--- package.bat
+++ package.bat
@@ -1,12 +1,16 @@
 @echo off
+echo Packaging...
 echo Package: > package.log
 echo ======== > package.log
+
+call build.bat > package.log
 
 FOR %%a in (
     "codegen.exe"
     "formats"
     "*.json"
 ) DO (
-    xcopy /s /y /f "%%~a" c:\WF\lp\tools\WeborbCodegen\libs\godegen\ >> package.log
+    xcopy /s /y /f "%%~a" c:\WF\lp\tools\WeborbCodegen\libs\codegen\ >> package.log
     echo: >> package.log
 )
+echo Done. See package.log for details
